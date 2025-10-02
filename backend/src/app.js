a9692c163611513e4193f178ce69ab7ab402ed08
Prince Babu <princebabu4495@gmail.com>
2025-10-02 22:11:50 +0530

    change links of deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://kaveri-living.vercel.app/",
     credentials: true,
 }));
 // routes
